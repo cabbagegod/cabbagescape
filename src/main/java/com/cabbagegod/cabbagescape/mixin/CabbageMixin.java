5e22cbf5d6879778215f3cea6886739fdc3026fb
CabbageGod <cabbagegod.dev@gmail.com>
2022-03-04 06:16:15 -0500

    Random Changes

--- src/main/java/com/cabbagegod/cabbagescape/mixin/CabbageMixin.java
+++ src/main/java/com/cabbagegod/cabbagescape/mixin/CabbageMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class CabbageMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		Main.LOGGER.info("This line is printed by an example mod mixin!");
+		Main.LOGGER.info("yo");
 	}
 }
