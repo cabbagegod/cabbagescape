87fc8c56a6fb773c27ab16787de1bb6934d7566b
CabbageGod <cabbagegod.dev@gmail.com>
2022-06-02 19:41:29 -0400

    Fixed potential issue with showing new update message

--- src/main/java/com/cabbagegod/cabbagescape/data/Settings.java
+++ src/main/java/com/cabbagegod/cabbagescape/data/Settings.java
@@ -1,10 +1,12 @@
 package com.cabbagegod.cabbagescape.data;
 
+import com.cabbagegod.cabbagescape.client.CabbageScapeClient;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Settings {
-    public String lastVersion = "";
+    public String lastVersion = CabbageScapeClient.version;
     public GroundItemSettings groundItemSettings = new GroundItemSettings();
     public NotificationSettings notificationSettings = new NotificationSettings();
 
